f5d15022070aa3e58e136a91dbd1d4dcbbafdf1d
sohaib alam <sohaib.alam3771@gmail.com>
2024-12-20 07:08:50 -0800

    View,Text,Button

---
+++ App.jsx
@@ -0,0 +1,20 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+import { Button, Text, View } from 'react-native';
+import React from 'react'
+
+const App = () => {
+  return (
+    <View>
+      <Text style={{ fontSize: 30 }}>This is my react native Application</Text>
+      <Text style={{ fontSize: 30 }}>This is my react native Application</Text>
+      <Button title='Press Here'/>
+    </View>
+  )
+};
+
+export default App
