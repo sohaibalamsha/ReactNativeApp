5bf3b61322b3c630e63c69433bddfeccffa1b17d
sohaib alam <sohaib.alam3771@gmail.com>
2024-12-26 14:02:51 -0800

    components calling

--- App.jsx
+++ App.jsx
@@ -6,13 +6,17 @@
  */
 import { Button, Text, View } from 'react-native';
 import React from 'react'
+import First from './src/First';
+import Second from './src/Second';
 
 const App = () => {
   return (
     <View>
       <Text style={{ fontSize: 30 }}>This is my react native Application</Text>
-      <Text style={{ fontSize: 30 }}>This is my react native Application</Text>
-      <Button title='Press Here'/>
+      <Text style={{ fontSize: 30 }}>This is my react native App</Text>
+      <Button title='Press here'/>
+      <First/>
+      <Second/>
     </View>
   )
 };
---
+++ src/First.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const First = () => {
+  return (
+    <View>
+      <Text style={{fontSize:30}}>First Component</Text>
+    </View>
+  )
+}
+
+export default First
